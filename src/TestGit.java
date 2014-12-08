fc83912cbc08cb09b11f65fda9863feb56632be3
Amjad Ullah <amjad.qau@gmail.com>
2014-12-08 00:01:14 +0000

    Test project
    Trying to learn how to use GitHub

---
+++ src/TestGit.java
@@ -0,0 +1,12 @@
+
+public class TestGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am trying to understand that how to use GitHub");
+	}
+
+}
